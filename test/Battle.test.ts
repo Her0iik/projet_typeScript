b8cdbb474bf25292165eedfc7c4f645f70f4902a
Thomas Gianfalla <44254960+Her0iik@users.noreply.github.com>
2020-03-30 17:44:20 +0200

    add ascyn fight

--- test/Battle.test.ts
+++ test/Battle.test.ts
@@ -1,28 +1,24 @@
-import {Battle} from "../src/Battle";
 import {Pokemon} from "../src/Pokemon";
 import {Attack} from "../src/Attack";
+import {Battle} from "../src/Battle";
 
-describe('Test Pokemon.ts', () => {
-
+describe('Test Battle.ts', () => {
     const attacks = [new Attack("charge", 40,  "physique", "normal", 100)]
     //const pikachu: Pokemon = new Pokemon("pikachu", 1,"Electrik",  attacks );
     const salameche: Pokemon = new Pokemon("salameche", 2, "Feu", 39, 43, 50, 52, 65, attacks);
     const Bulbizarre: Pokemon = new Pokemon("Bulbizarre", 3, "Herbe", 45, 49, 65, 49, 45, attacks);
     const Carapuce: Pokemon = new Pokemon("Carapuce", 4, "Eau", 44, 65, 64, 48, 43, attacks);
 
-
-    test('salameche should have 39 hp', () => {
-        Bulbizarre.attack(Bulbizarre, 0, true);
-        expect(salameche.hp).toBe(39);
+    test('Bulbizarre should be 1st', () => {
+        expect(Battle.attackFirst(Bulbizarre, Carapuce)).toEqual([Bulbizarre,Carapuce]);
     });
 
-    test('Carapuce should have 41 hp', () => {
-        Bulbizarre.attack(Carapuce, 0, true);
-        expect(Carapuce.hp).toBe(41);
+    test('Salameche should be 1st', () => {
+        expect(Battle.attackFirst(salameche, Bulbizarre)).toEqual([salameche,Bulbizarre]);
     });
+/*
+    test('Battle should get salameche as winner', async () => {
+        expect(await Battle.fight(salameche, Bulbizarre)).toEqual(salameche);
+    });*/
 
-    test('Carapuce should lost 0 hp and stay at 41 hp', () => {
-        Bulbizarre.attack(Carapuce, 0, false);
-        expect(Carapuce.hp).toBe(41);
-    });
 });
